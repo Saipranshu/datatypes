49d17ccc0d0e2d4f97436c31246a4257dfe9bd3e
Saipranshu <venkateshadb@yahoo.com>
2024-10-28 19:15:09 +0530

    py 1

---
+++ main.py
@@ -0,0 +1,8 @@
+name="Sai Pranshu"
+age=15
+is_student=True
+weight=41.6
+print("Data type of name is ",type(name))
+print("Data type of age is ",type(age))
+print("Data type of is_student is ",type(is_student))
+print("Data type of weight is ",type(weight))
